30a2776f522661a2cfdfc336b02a3bf0ad340cdd
Delano <lamdela@myvuw.ac.nz>
2024-02-01 11:01:01 +1300

    Working API getAllContacts using Express framework

--- back-end/server.js
+++ back-end/server.js
@@ -1,13 +1,33 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = 5000;
+const mysql = require('mysql2');
 
 const bodyParser = require('body-parser');
 
-const main = require("./backMain.js");
+const db = mysql.createConnection({
+    host: "127.0.0.1",
+    user: "root",
+    password: "",
+    database: "grab_and_go"
+});
+db.connect((err) => {
+    if (err){
+        throw err;
+    }
+    console.log('Connected to DB');
+});
+
+// const main = require("./backMain.js");
+
+//If request received for '/getAllContacts, execute file contacts.js
+const contacts = require('./getAllContacts'); //require('file name')
+app.use('/api/getAllContacts', contacts) //use ('API msg pass')
+
+
 
 app.use(bodyParser.json());
-app.use("/getAllContacts", main);
+// app.use("/getAllContacts", main);
 
 app.listen(PORT, (app) => {
     console.log("pls work");
