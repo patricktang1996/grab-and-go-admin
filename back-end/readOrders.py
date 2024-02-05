0b407ac479b875ade7584a4739e659fb1dcfc8c3
rosie <rosina.jane.wyllie@gmail.com>
2024-02-05 15:30:22 +1300

    updated code to insert data into UPDATED database table format, tested code by running the orders.txt file which contains 1 order, further testing required, need data from client.

--- back-end/readOrders.py
+++ back-end/readOrders.py
@@ -16,7 +16,10 @@ for table in cursor:
 
 
 #hard coded file location
-csv_Job_Orders = 'C:\\xampp\\htdocs\\myphpfiles\\GrabAndGo\\Orders.txt'
+
+#csv_Job_Orders = 'GRAB-AND-GO-ADMIN\\back-end\\Orders.txt'
+csv_Job_Orders = 'C:\\Users\\wyllierosi\\Documents\\504\\grab-and-go-admin\\back-end\\Orders.txt'
+
 def readOrders():
     with open(csv_Job_Orders, 'r') as csv_file:
         csv_reader = csv.DictReader(csv_file)
@@ -25,51 +28,98 @@ def readOrders():
         for _ in range(18):
             next(csv_reader, None)
 
+        generatedInt = 1
         for order in csv_reader:
-            job_order = order['Job']
+            #job_order is job number same as packing slip number
+            job_order = order['job number']
             date = order['Date']
             company = order['Company']
             POnumber = order['PO Number']
             #OnePersonKits: Spaces needs to be changed to normal spacebar character, delete ending space
-            OnePersonKits = order['1 Person Kits'] 
-            FourPersonDuffles = order['4 person Duffles']
-            DynamoTorch = order['Dynamo Torches']
-            SurfLifesavingKits = order['Surf Lifesaving Kits']
-            SixtyfivePieceFirstAid = order['65 piece First Aid']
-            WaterproofPlus= order['Waterproof Plus - Plain ']
-            PremiumFirstAidKit= order['Premium First Aid Kit']
-            CompactFirstAidKit= order['Compact First Aid Kits']
-            SolarLanterns= order['Solar Lanterns']
-            WaterproofSolarTorch= order['Waterproof Solar Torch']
-            Powerbank10000= order['Powerbank 10000']
-            Powerbank15000= order['Powerbank 15000']
-            KN95Masks= order['KN95 Masks']
-            Gloves= order['Gloves']
-            SpecialItems= order['Special Items']
+            
+            #query customer_contact_information table to retreive company id with organisation_name(company)
+            sqlQueryForCompanyId = "SELECT id FROM customer_contact_information WHERE organisation_name = %s"
+            companyname = (company, )
+            cursor.execute(sqlQueryForCompanyId, companyname)
+            #fetchall() fetches all rows from the last executed statement
+            #do 
+            companyIdResults = cursor.fetchall()
+            # Printing all records or rows from the table. 
+            # It returns a result set.  
+            companyIdString = int(0)
+            companyId = int(0)
+            for all in companyIdResults: 
+                print(all)
+                companyIdresult = companyIdResults[0]
+                companyId = companyIdresult[0]
+                print(companyId)
+
+            #if product has value/is not null then insert into order details
+            #value returns quantity of product
+                
+            #1 Person kits title MUST BE CHANGED to 1 Person Emergency Kit
+            #Surf Lifesaving Kits,65 piece First Aid,Waterproof Plus - Plain ,Premium First Aid Kit,Compact First Aid Kits,Solar Lanterns,Waterproof Solar Torch,Powerbank 10000,Powerbank 15000,KN95 Masks,Gloves
+            productList = ["1 Person Emergency Kit", "4 person Duffles", "Dynamo Torches", "Surf Lifesaving Kits", "65 piece First Aid", "Waterproof Plus - Plain", "Premium First Aid Kit", "Compact First Aid Kits", "Solar Lanterns", "Waterproof Solar Torch", "Powerbank 10000", "Powerbank 15000", "KN95 Masks", "Gloves"]
+            for product in productList:
+                
+                if product in order and order[product]:
+                    productQuantityResult = order[product]
+                    #pass quantity string to integer
+                    productQuantity = int(productQuantityResult)
+
+                
+                    #to get product_id query products table provding name to return matching ID
+                    sqlQueryForProductId = "SELECT id FROM products WHERE name = %s"
+                    value = (product, )
+                    cursor.execute(sqlQueryForProductId, value)
+                    productIdResults = cursor.fetchall()
+                    #retreive item from list
+                    productId = productIdResults[0]
+                    #retreive item from tuple
+                    productIdresult = productId[0] 
+                    #convert item to type int
+                    productIdInt = int(productIdresult)
+                
+                
+                    #get price for product, query pricing_details table with product_id(productId) and company_id to retreive correct price. 
+                    sqlQueryForPrice = "SELECT price FROM pricing_details WHERE product_id = %s AND company_id = %s"
+                    valuesForSqlPrice = (productIdInt, companyId)
+                    cursor.execute(sqlQueryForPrice, valuesForSqlPrice)
+                    priceResults = cursor.fetchall()
+                    priceList = priceResults[0]
+                    #retreive item from tuple
+                    priceResult = priceList[0]
+                    #convert item to type int
+                    priceInt = int(priceResult)
+
+                    #insert 1PK details into order_details table in 
+                    sqlQueryInsertOrder_details = "INSERT INTO order_details (order_id, product_id, quantity, price) VALUES (%s, %s, %s, %s)"
+                    valuesToInsert = (job_order, productIdInt, productQuantity, priceInt)
+                    cursor.execute(sqlQueryInsertOrder_details, valuesToInsert)
+                    mydb.commit()
+                    print (product + " --has been inserted into DB.")
+                
+                else:
+                    print ("no --"+ product + "-- in order.")
+                #amend '1 person kits' header to use normal spacebar and remove end space
+
             #Converting any input into True
             Delivered = 1 if order['Delivered'] else 0
             BarlowsInvoice = 1 if order['Barlows Invoice'] else 0
             Invoiced = 1 if order['Invoiced'] else 0
             Paid = 1 if order['Paid'] else 0
-
-            # print(job_order, date, company, POnumber, OnePersonKits, FourPersonDuffles, DynoamoTorch, 
-            #     SurfLifesavingKits, SixtyfivePieceFirstAid, WaterproofPlus, PremiumFirstAidKit, 
-            #     CompactFirstAidKit, SolarLanterns,WaterproofSolarTorch, Powerbank10000, Powerbank15000, 
-            #     KN95Masks, Gloves,SpecialItems, Delivered, BarlowsInvoice, Invoiced, Paid)
             
-            # query = "INSERT INTO job_orders (job_order, date_ordered, company_name, purchase_order_number,\
-            #     special_delivery_instructions, 1_person_kit, 4_person_duffle, Dynamo_Torches, surf_lifesaving_kit,\
-            #     65_piece_first_aid, premium_first_aid_kit, compact_first_aid_kit, solar_lantern,\
-            #     waterproof_solar_torch, powerbank_10000, powerbank_15000, KN95_masks,\
-            #     gloves, special_items, delivered, barlows_invoice, invoiced, paid) \
-            #     VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
-            # values = (job_order, date, company, POnumber, "", OnePersonKits, FourPersonDuffles, DynamoTorch, 
-            #     SurfLifesavingKits, SixtyfivePieceFirstAid, WaterproofPlus, PremiumFirstAidKit, 
-            #     CompactFirstAidKit, SolarLanterns, WaterproofSolarTorch, Powerbank10000, Powerbank15000, 
-            #     KN95Masks, Gloves,SpecialItems, Delivered, BarlowsInvoice, Invoiced, Paid)
-            cursor.execute(query, values)
-            cursor.commit()
-            cursor.nextset()
+            sqlQueryForOrders = "INSERT INTO orders (job_number, purchase_order, customer_id, date, freight_cost, invoiced_barlows, delivered, invoiced_myob, paid) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
+            valuesForOrders = (job_order, POnumber, generatedInt, date, 0.00, BarlowsInvoice, Delivered, Invoiced, Paid)
+
+            
+
+            generatedInt +1
+            cursor.execute(sqlQueryForOrders, valuesForOrders)
+            mydb.commit()
+            
+            
+            
 readOrders()
             
 cursor.close()
