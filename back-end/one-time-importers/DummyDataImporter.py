81360b0ef616bca19091818a17ccd70b6ba97530
Kade Jewiss <jewisskade@ecs.vuw.ac.nz>
2024-02-12 13:40:54 +1300

    moved importers all into one file

--- back-end/one-time-importers/DummyDataImporter.py
+++ back-end/one-time-importers/DummyDataImporter.py
@@ -239,3 +239,6 @@ if __name__ == "__main__":
     random_pricing(database, cursor)
     random_orders(database, cursor)
     random_tags(database, cursor)
+    
+    database.close()
+    cursor.close()
